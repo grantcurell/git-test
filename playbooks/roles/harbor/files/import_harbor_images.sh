0ed068bfcbcab85d4e3102fdfc5940fa8a114026
Grant Curell <grantcurell@gmail.com>
2019-03-25 17:40:10 -0500

    AARON

--- playbooks/roles/harbor/files/import_harbor_images.sh
+++ playbooks/roles/harbor/files/import_harbor_images.sh
@@ -28,7 +28,37 @@ if [ "$(docker images | grep -v ${HOSTNAME} 2> /dev/null)" ]; then
   x=1
   HOSTNAME="$(hostname)"
 
-  # TOTHIS DOESN"T LOOK IMPORTANT. DELETE
+  # TODO: Should definitely check they are up instead of waiting for 20 seconds.
+  sleep 20
+  # TODO: need to delete out the images not in use by docker and the old images
+  if [ "$(ping -c 1 ${HOSTNAME})" ]; then
+    # Log in to harbor
+    # TODO: When time permits, we should add a function to perform hostname verification to make sure the host is in format XXXX.YYYY
+    docker login --username="admin" --password="password" "${HOSTNAME}"
+
+    # TODO: Delete out the long name images
+
+    # Here I am grabbing each image that doesn't have a tag of the repo name and then getting the third column which is the image ID
+    for image in $(docker images | grep -v ${HOSTNAME} | awk '{print $3}' | grep -v IMAGE); do
+
+      # Get the name of the image that is associated with the respective image
+      # number from the above for loop
+      name=$(docker images | grep -v ${HOSTNAME} | awk '{print $1}' | grep -v REPOSITORY | sed -n "${x}p")
+
+      # Bash speak for increment x by 1
+      let "x++"
+
+      # Retag all the images for the registry. Format hostname/project_name/name:version
+      docker tag "${image}" "${HOSTNAME}/library/$name:latest"
+
+      # Push the image to Harbor
+      docker push "${HOSTNAME}/library/$name:latest"
+    done
+  fi
+fi
+}
+
+#Script Execution:
 ########################
 main
 
